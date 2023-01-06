6c1a79e9a27bb66bcae97a4f0a4803961138cdc5
Adam Pluciński <adaskothebeast@gmail.com>
2023-01-06 02:26:42 +0100

    fix for tests and sample launching

--- test/unit/AdaskoTheBeAsT.Identity.Dapper.SqlClient.Test/Snapshots/SqlIdentityDapperSourceGeneratorSnapshotTest.GeneratesSqlCorrectlyAsync#IdentityUserSql.g.verified.cs
+++ test/unit/AdaskoTheBeAsT.Identity.Dapper.SqlClient.Test/Snapshots/SqlIdentityDapperSourceGeneratorSnapshotTest.GeneratesSqlCorrectlyAsync#IdentityUserSql.g.verified.cs
@@ -8,21 +8,21 @@ namespace AdaskoTheBeAsT.Identity.Dapper.Sample
     {
         public string CreateSql { get; } =
             @"INSERT INTO dbo.AspNetUsers(
-UserName
-,NormalizedUserName
-,Email
-,NormalizedEmail
-,EmailConfirmed
-,PasswordHash
-,SecurityStamp
-,ConcurrencyStamp
-,PhoneNumber
-,PhoneNumberConfirmed
-,TwoFactorEnabled
-,LockoutEnd
-,LockoutEnabled
-,AccessFailedCount
-,IsActive)
+[UserName]
+,[NormalizedUserName]
+,[Email]
+,[NormalizedEmail]
+,[EmailConfirmed]
+,[PasswordHash]
+,[SecurityStamp]
+,[ConcurrencyStamp]
+,[PhoneNumber]
+,[PhoneNumberConfirmed]
+,[TwoFactorEnabled]
+,[LockoutEnd]
+,[LockoutEnabled]
+,[AccessFailedCount]
+,[IsActive])
 VALUES(
 @UserName
 ,@NormalizedUserName
@@ -40,5 +40,25 @@ VALUES(
 ,@AccessFailedCount
 ,@Active);
 SELECT SCOPE_IDENTITY();";
+
+        public string UpdateSql { get; } =
+            @"UPDATE dbo.AspNetUsers
+SET [UserName]=@UserName
+,[NormalizedUserName]=@NormalizedUserName
+,[Email]=@Email
+,[NormalizedEmail]=@NormalizedEmail
+,[EmailConfirmed]=@EmailConfirmed
+,[PasswordHash]=@PasswordHash
+,[SecurityStamp]=@SecurityStamp
+,[ConcurrencyStamp]=@ConcurrencyStamp
+,[PhoneNumber]=@PhoneNumber
+,[PhoneNumberConfirmed]=@PhoneNumberConfirmed
+,[TwoFactorEnabled]=@TwoFactorEnabled
+,[LockoutEnd]=@LockoutEnd
+,[LockoutEnabled]=@LockoutEnabled
+,[AccessFailedCount]=@AccessFailedCount
+,[IsActive]=@Active
+WHERE Id=@Id
+;";
     }
 }
